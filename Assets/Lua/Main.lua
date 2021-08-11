0d91ab8753387c49438b94ea2393f262fc2a4143
Jonny <jonny.hong91@gmail.com>
2021-08-12 01:42:43 +0800

    【洪金敏】数据绑定 demo修改

--- Assets/Lua/Main.lua
+++ Assets/Lua/Main.lua
@@ -1,17 +1,23 @@
 local binder = require "Binder"
+local model = require "Model"
+
+package.cpath = package.cpath .. ';C:/Users/Jonny/AppData/Local/JetBrains/Toolbox/apps/Rider/ch-0/211.7628.61.plugins/EmmyLua/classes/debugger/emmy/windows/x64/?.dll'
+local dbg = require('emmy_core')
+dbg.tcpConnect('localhost', 9966)
+
 
 function Main()
+    local role_data = {
+        ["name"] = "role_1",
+        ["hobby"] = "sleep",
+        ["age"] = 1,
+    }
 
-    --local t = binder.data({ x = 123 })
-    --binder.bind(t, "x", function(val, old)
-    --    print(old .. " => " .. val)
-    --end)
-    --
-    --t.x = 12
-    --t.x = 123
+    model:add(1, binder.data(role_data))
 
     local panel = require("SamplePanel")
     panel:Load()
+    panel:Refresh(model:get(1))
     panel:Start()
 end
 
